0497d558ea2b8f0238bb07920b1c5c1430ab56ef
Tufan Calisir <tufan.calisir@project-joker.com>
2023-07-27 16:49:33 +0200

    fix: yarn build

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import { getStoryblokApi, StoryblokComponent } from '@storyblok/react/rsc';
 
 async function fetchData() {
-	let sbParams = { 
-    version: 'draft' as const 
-  };
+	let sbParams = {
+		version: 'draft' as const,
+	};
 
 	const storyblokApi = getStoryblokApi();
-	return storyblokApi.get(`cdn/stories/contact`, sbParams);
+	return storyblokApi.get(`cdn/stories/home`, sbParams);
 }
-
 
 export default async function Home() {
 	const { data } = await fetchData();
